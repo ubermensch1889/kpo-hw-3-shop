4f577b8458f867018aba03fd60055135db262860
Erentsen <sangadzhiev1406@mail.ru>
2024-06-06 13:29:46 +0300

    add entities

---
+++ src/main/java/hse/kpo/hw/kpohw3shop/entity/Order.java
@@ -0,0 +1,40 @@
+package hse.kpo.hw.kpohw3shop.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.sql.Timestamp;
+
+@Entity
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "order")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    enum Status {
+        CHECK,
+        SUCCESS,
+        REJECTION
+    }
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "from_station_id", nullable = false)
+    private Station fromStation;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "to_station_id", nullable = false)
+    private Station toStation;
+
+    @Column(nullable = false)
+    private Timestamp created;
+
+    @Enumerated(EnumType.ORDINAL)
+    @Column(nullable = false)
+    private Status status;
+}
