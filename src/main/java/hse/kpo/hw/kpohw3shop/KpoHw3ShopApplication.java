37d6f849a6dfb7a4ba2f25fcd6f18a99a282aadd
Erentsen <sangadzhiev1406@mail.ru>
2024-06-06 12:50:06 +0300

    initial commit

---
+++ src/main/java/hse/kpo/hw/kpohw3shop/KpoHw3ShopApplication.java
@@ -0,0 +1,13 @@
+package hse.kpo.hw.kpohw3shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KpoHw3ShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KpoHw3ShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hse/kpo/hw/kpohw3shop/KpoHw3ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package hse.kpo.hw.kpohw3shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KpoHw3ShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
