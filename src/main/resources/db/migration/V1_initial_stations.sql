ca6d4e9a4f924df308b258d25a952f1445ba8126
Erentsen <sangadzhiev1406@mail.ru>
2024-06-08 14:46:14 +0300

    add migration for data initializing

---
+++ src/main/resources/db/migration/V1_initial_stations.sql
@@ -0,0 +1,4 @@
+INSERT INTO station (station_id, station) VALUES (1, 'Hogwarts');
+INSERT INTO station (station_id, station) VALUES (2, 'London');
+INSERT INTO station (station_id, station) VALUES (3, 'Elista');
+
